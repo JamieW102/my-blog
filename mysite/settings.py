d62d4fa2c90c1d474379a5b37bfa55c156206dd9
JamieW102 <jamie.white1002@gmail.com>
2020-05-26 19:07:55 +0100

    Finishing touches to styling.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bmp9@sls!*cu_#_hbj9bjthcqkqlf979ijc0nsrk6(1k$2=4)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
