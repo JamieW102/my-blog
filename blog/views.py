bd2c88451f7ba9663f8aefd5a03ee29506d5308e
JamieW102 <jamie.white1002@gmail.com>
2020-05-27 14:51:21 +0100

    Added a home page.

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def home(request):
+	return render(request, 'blog/index.html')
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
